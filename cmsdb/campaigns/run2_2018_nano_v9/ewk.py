87aeb1b6ba2248070a34e98aae46ddbba44808f5
juvanden <jules.vandenbroeck@cern.ch>
2024-10-08 10:41:54 +0200

    change zz_zll_zll dataset name

--- cmsdb/campaigns/run2_2018_nano_v9/ewk.py
+++ cmsdb/campaigns/run2_2018_nano_v9/ewk.py
@@ -158,9 +158,9 @@ cpn.add_dataset(
 #
 
 cpn.add_dataset(
-    name="zzto4l_powheg",
+    name="zz_zll_zll_powheg",
     id=14241148,
-    processes=[procs.zz_llll],
+    processes=[procs.zz_zll_zll],
     keys=[
         "/ZZTo4L_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM",  # noqa
     ],
