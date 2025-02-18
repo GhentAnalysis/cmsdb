24870759590647c4e74f1c9b6f3d4c25509bbfef
juvanden <jules.vandenbroeck@cern.ch>
2025-02-18 12:15:18 +0100

    changed nanoAOD version to integer

--- cmsdb/campaigns/run2_2018_nano_v9_pNet/__init__.py
+++ cmsdb/campaigns/run2_2018_nano_v9_pNet/__init__.py
@@ -14,7 +14,7 @@ cpn = campaign_run2_2018_nano_v9_pNet = Campaign(
         "run": 2,
         "tier":
         "NanoAOD",
-        "version": "9",
+        "version": 9,
         "postfix": "",
     },
 )
