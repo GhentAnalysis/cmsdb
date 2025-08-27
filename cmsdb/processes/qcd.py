7a1ab4fbb6dfbab3fd4956a1775a88e0a6eb7208
JulesVandenbroeck <93740577+JulesVandenbroeck@users.noreply.github.com>
2025-08-27 16:32:50 +0200

    Remove duplicate QCD process definitions
    Duplicate process defintion bgen pt binned qcd

--- cmsdb/processes/qcd.py
+++ cmsdb/processes/qcd.py
@@ -187,62 +187,6 @@ qcd_ht2000toinf = qcd.add_process(
     },
 )
 
-qcd_bgen_ht200to300 = qcd.add_process(
-    name="qcd_bgen_ht200to300",
-    id=33301,
-    xsecs={
-        # https://cms.cern.ch/iCMS/jsp/db_notes/noteInfo.jsp?cmsnoteid=CMS%20AN-2019/197 (v3)
-        13: Number(111700),
-    },
-)
-qcd_bgen_ht300to500 = qcd.add_process(
-    name="qcd_bgen_ht300to500",
-    id=33302,
-    xsecs={
-        # https://cms.cern.ch/iCMS/jsp/db_notes/noteInfo.jsp?cmsnoteid=CMS%20AN-2019/197 (v3)
-        13: Number(27960),
-    },
-)
-qcd_bgen_ht500to700 = qcd.add_process(
-    name="qcd_bgen_ht500to700",
-    id=33303,
-    xsecs={
-        # https://cms.cern.ch/iCMS/jsp/db_notes/noteInfo.jsp?cmsnoteid=CMS%20AN-2019/197 (v3)
-        13: Number(3078),
-    },
-)
-qcd_bgen_ht700to1000 = qcd.add_process(
-    name="qcd_bgen_ht700to1000",
-    id=33304,
-    xsecs={
-        # https://cms.cern.ch/iCMS/jsp/db_notes/noteInfo.jsp?cmsnoteid=CMS%20AN-2019/197 (v3)
-        13: Number(732),
-    },
-)
-qcd_bgen_ht1000to1500 = qcd.add_process(
-    name="qcd_bgen_ht1000to1500",
-    id=33305,
-    xsecs={
-        # https://cms.cern.ch/iCMS/jsp/db_notes/noteInfo.jsp?cmsnoteid=CMS%20AN-2019/197 (v3)
-        13: Number(139),
-    },
-)
-qcd_bgen_ht1500to2000 = qcd.add_process(
-    name="qcd_bgen_ht1500to2000",
-    id=33306,
-    xsecs={
-        # https://cms.cern.ch/iCMS/jsp/db_notes/noteInfo.jsp?cmsnoteid=CMS%20AN-2019/197 (v3)
-        13: Number(14.7),
-    },
-)
-qcd_bgen_ht2000 = qcd.add_process(
-    name="qcd_bgen_ht2000",
-    id=33307,
-    xsecs={
-        # https://cms.cern.ch/iCMS/jsp/db_notes/noteInfo.jsp?cmsnoteid=CMS%20AN-2019/197 (v3)
-        13: Number(3.09),
-    },
-)
 #
 # QCD BGen HT-binned
 #
