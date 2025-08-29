da363510e76a9ab6988c33dafc46c1ac760a78ad
juvanden <jules.vandenbroeck@cern.ch>
2025-08-29 17:58:57 +0200

    update of dy_m50toinf_2j_pt600_amcatnlo dataset process

--- cmsdb/campaigns/run3_2024_nano_v15/ewk.py
+++ cmsdb/campaigns/run3_2024_nano_v15/ewk.py
@@ -191,7 +191,7 @@ cpn.add_dataset(
 cpn.add_dataset(
     name="dy_m50toinf_2j_pt600_amcatnlo",
     id=14679255,
-    processes=[procs.dy_m50toinf_2j_pt600],
+    processes=[procs.dy_m50toinf_2j_pt600toinf],
     keys=[
         "/DYto2L-2Jets_Bin-2J-MLL-50-PTLL-600_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa
     ],
