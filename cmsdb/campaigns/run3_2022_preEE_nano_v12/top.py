331efa84900a36a5dd995ef13f4c906c948db191
Jules Vandenbroeck <Jules.Vandenbroeck@cern.ch>
2025-11-13 15:37:49 +0100

    fixed n_files for tt_semileptonic and added broken file

--- cmsdb/campaigns/run3_2022_preEE_nano_v12/top.py
+++ cmsdb/campaigns/run3_2022_preEE_nano_v12/top.py
@@ -23,8 +23,13 @@ cpn.add_dataset(
             keys=[
                 "/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
             ],
-            n_files=187,
+            n_files=125,
             n_events=87986940,
+            aux={
+                "broken_files": [
+                    "/store/mc/Run3Summer22NanoAODv12/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/NANOAODSIM/130X_mcRun3_2022_realistic_v5-v2/30000/751ae3ca-c0ce-4b19-a771-13820553a706.root",  # noqa
+                ],
+            },
         ),
         extension=DatasetInfo(
             keys=[
